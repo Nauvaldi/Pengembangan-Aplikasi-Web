168563b231a13b9fb3ebf1a1b198101deeb5c29d
Muhammad Nauvaldi <muhammad.nauvaldi23@gmail.com>
2022-08-30 20:33:39 +0700

    Add files via upload

---
+++ Tugas PAW Output Pertemuan 2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+	function family($nama,$tgl,$tempat){
+		echo ("Nama : $nama<br/>");
+		echo ("Lahir $tgl di $tempat <br/>");
+	}
+	echo ("Data Anggota Keluarga");
+	echo ("<hr>");
+	family("Muhammad Nauvaldi Akbar Alfaruqi","06 Oktober 2001","Kediri");
+	echo ("<br>");
+	family("Umsiyah","12 April 1968","Kediri");
+	echo ("<br>");
+	family("Tukidjo","26 Juli 1968","Surabaya");
+	echo ("<br>");
+?>
+</body>
+</html>
+	
+	
